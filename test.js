ba2b8b80539dc3870e8083ad7de2a8417d3df67d
Aleksandr Shaytanov <rdischain@yandex.ru>
2017-07-19 19:02:22 +0400

    Update test.js

--- test.js
+++ test.js
@@ -1,89 +1,116 @@
-'use strict';
+function Algorithm() {
+  this._running = false;
+  this._paused = false;
+  this._stopped = false;
+}
 
-/**
- * A basic Async Task, which may invoke given `asyncFunc` function with given `args` and  * `userCallback`.
- */
-function AsyncTask(args, asyncFunc, userCallback) {
-  this._args = args;
-  this._function = asyncFunc;
-  this._userCallback = userCallback;
+Algorithm.prototype.run = function(cb) {}
+
+Algorithm.prototype.pause = function(cb) {}
+
+Algorithm.prototype.stop = function(cb) {}
+
+Algorithm.prototype.restart = function() {}
+
+function clone(obj) {
+
+  if (Array.isArray(obj)) {
+
+    let copy = [];
+
+    for (let i = 0; i < obj.length; i++) {
+
+      let value = obj[i];
+
+      copy[i] = (value !== null && typeof value === "object") ? clone(value) : value;
+
+    }
+  } else {
+
+    let copy = {};
+
+    for (let key in obj) {
+
+      if (obj.hasOwnProperty(key)) {
+
+        let value = obj[key];
+
+        copy[key] = (value !== null && typeof value === "object") ? clone(value) : value;
+
+      }
+
+    }
+
+  }
+
+  return copy;
+
 }
 
 /**
- * Run `asyncFunc` with given `args` and `userCallback` and finally invokes
- * some not stricltly required `finalCallback`
- *
- * @param{Function} finalCallback
+ * Extends <Algorithm>
+ * Not changes the initial `container`
  */
-AsyncTask.prototype.run = function(finalCallback) {
-  // This `callback` should be invoked with target async function call and it
-  // contains `_userCallback` invocation at its body
-  let callback = function() {
-    let results = Array.prototype.slice.call(arguments);
-    this._userCallback(results);
-    
-    if (finalCallback) finalCallback();
-  };
 
-  callback = callback.bind(this);
-
-  // To implement this, simply append `callback` to the end arguments list
-  this._args.push(callback);
-  
-  this._function.apply(this, this._args);
+function ParallelLimited(container, limit) {
+  Algorithm.call(this);
+   // контейнеры отдать суперклассу
+  this._container = clone(container);
+  this._copyContainer =  clone(container);
+  this._limit = limit;
 }
 
-function PrioritisedAsyncTask(args, asyncFunc, userCallback, priority) {
-  let temp = Array.prototype.slice.call(arguments); temp.pop();
-  AsyncTask.apply(this, temp);
+/**
+ * `cb` should be executed after all async tasks complete.
+ */
+ParallelLimited.prototype.run = function(cb) {
+  let running = 1,
+       curTask = this._container.next();
+  function next() {
+    --running;
 
-  this._priority = priority;
+    if (!task && running === 0) {
+      cb();
+    }
+    // Заменить this._container.length на hasMore() в каждой коллекции (метод интерфейса)
+    while(running < this._limit && this._container.length > 0) {
+      (function (task) {
+        task.run(next);
+      })(curTask);
+      curTask = this._container.next();
+      running++;
+    }
+  }
+  next();
 }
 
-PrioritisedAsyncTask.prototype = Object.create(AsyncTask.prototype);
-PrioritisedAsyncTask.prototype.constructor = PrioritisedAsyncTask;
-
-PrioritisedAsyncTask.prototype.getPriority = function() { return this._priority; };
-
-//-------------- Read File Example---------------------//
-function read(path ,callback) {
-  require('fs').readFile(path, callback);
+Parallel.prototype.run = function(cb) {
+  this._items.forEach((item, index) => {
+    if (index === this._items.size()) {
+      item.run(cb);
+    } else {
+      item.run();
+    }
+  });
 }
-function user_callback(data) { console.log(data.toString()); }
-//-----------------------------------------------------------//
 
-// AsyncTask
-// let task = new AsyncTask(['test.js'], read, user_callback);
-// task.run(() => { console.log('task success'); });
+Series.prototype.run = function(cb) {
+  function next() {
+    let curTask = this._container.next();
+    if (this._container.hasNext()) {
+      curTask.run(next);
+    } else {
+      curTask.run(cb);
+    }
+  }
+}
 
-// PrioritisedAsyncTask
-let put = new PrioritisedAsyncTask(['test.js'], read, user_callback, 1);
-console.log(put.getPriority());
-put.run(() => { console.log('task success'); });
+// В AsyncQueue и AsyncPQ:
+Async*.prototype.parallelLimited = function(limit) {
+  this._algorithm = new ParallelLimited(this._tasks, limit);
+  return this;
+}
 
-// XHR Example
-// let u = new UniqueTask(['/', true], xhr, () => console.log('fdf'));
-
-// function xhr(recourse, isAsync) {
-//   let x = new XMLHttpRequest();
-//    x.open('GET', recourse, isAsync);
-//    x.onreadystatechange = function(data) {
-//      if (x.readyState ==4)
-//        console.log(x.responseText); 
-//    };
-//
-//    x.send();
-// }
-
-// Дописать в PQ, что items - экземпляры класса PrioritisedUniqueTask
-// 1). Сначала инициализируется AsyncQueue или AsyncPriorityQueue.
-// 2). Потом аргументы конструктора превращаются в массив AsyncTask или PrioritisedAsyncTask
-// 3). После этого они отправляются в конструктор Queue либо PQ.
-// 4). После инициализации тасками, вызывается один из методов, унаследованных от AsynRunner - 
-//      serial, parallel и т.д. 
-// 5). Затем выбранный метод инициализирует соответствующий экземпляр алгоритма путем передачи
-//      ему экземпляра ранее инстанцированной Queue или PQ. Этот метод должен возвращать `this`
-//      AsyncRunner`a для реализации chaining`а.
-// 6). Теперь может вызвываться один из методов манипуляции асинхронным хранилищем - start(), stop(), 
-//       pause(). Если алгоритм не выбран, на этих методах будет генерироваться искдючение. Так же
-//       доступны предикаты running(), stoped(), paused().
+Async*.prototype.run = function(cb) {
+  this._algorithm.run(cb);
+}
