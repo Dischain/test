276a04a4cca805b092ce5a94a9c7c24dd55cceda
Aleksandr Shaytanov <rdischain@yandex.ru>
2017-08-14 19:25:01 +0400

    Update test.js

--- test.js
+++ test.js
@@ -1,38 +1,135 @@
 'use strict';
 
-const http = require('http')
-        , path = require('path')
-        , url    = require('url')
-        , fs      = require('fs');
+const path = require('path')
+    , url  = require('url')
+    , http = require('http');
 
-let publicDir = './public';
-//publicDir = path.normalize(path.resolve(root || '.'));
-const publicDirPath = path.resolve(publicDir);
+// Options:
+//
+// cache: {Boolean} - false by default
+// cacheHeaders: {String} - if not specified and if cache is true, the default max-age header 
+//                          should be 3600 seconds
+//
+// encoding: {Boolean} - accept encoding.
+// 
+// cors: {Boolean}
+// corsHeaders: {String}
+//
+// host: {String} - if not specified, default host should be `127.0.0.1`
+// port: {Number} - if not specified, default port should be 3000
+// root: {String} - if not specified, default path to directory is './'
+// name: {String}
+//
+// notFound: {String} - path to default 404 answer; should be placed into the `root`folder
 
-http.createServer(function (req, res) {
-  console.log(req.url);
-  const reqPath = url.parse(req.url).pathname;
-  console.log(reqPath);
-  const reqFullPath = path.join(publicDirPath, reqPath);
+const defaultOpts = {
+	cache: false,
+	cacheHeaders: 'private, max-age=3600',
+	encoding : false,
+	host: '127.0.0.1',
+	port: 3000,
+	root: './',
+	cors: false,
+	corsHeaders: '',
+	name: 'node.js'
+}
 
-  fs.exists(reqFullPath, function(exists) {
-    if (!exists) { 
-      res.statusCode = 404; 
-      res.end(`File ${reqFullPath} not foud, mother fucker! :)`);
-      return;
-    }
+function Server(opts) {
+	if (!opts) {
+		this.options = defaultOpts;
+	} else {
+		this.options = Object.keys(defaultOpts).reduce((init, key) => {
+			if (!opts[key] || opts[key] === '') {
+				init[key] = defaultOpts[key]
+			} else {
+				init[key] = opts[key];
+			}
+	
+			return init;
+		}, {});
+	}
 
-    /*if (fs.statSync(pathName).isDirectory()) {
-      reqFullPath += '/index.html';
-    }*/
+	this.options.root = path.resolve(path.normalize(this.options.root));
+	console.log(this.options)
+}
 
-    fs.readFile(reqFullPath, function(err, data) {
-      if (err) { res.statusCode(500); res.end(`Error getting file: ${err}`); }
-      else {
-        const ext  = path.parse(reqFullPath).ext;
+Server.prototype.start = function start(callback) {
+  this._server = http.createServer(handler(this))
+                     .listen(this.options.port, this.options.host, callback);
+};
 
-        res.end(data);
-      }
-    });
-  });
-}).listen(3000, function() { console.log('server is now  running on port 3000'); });
+Server.prototype.stop = function stop() {
+  if (this._server) {
+    this._server.close();
+    this._server = null;
+  }
+};
+
+function handler(server) {
+	return function(req, res) {
+		let pathname  = url.parse(req.url).pathname
+		  , fullPath = path.join(server.options.root, pathname);
+		
+		res.headers = {};
+		
+		if (server.options.name) {
+     		res.headers['X-Powered-By'] = server.options.name;
+    	}
+
+		if (server.options.cors) {
+    		res.headers['Access-Control-Allow-Origin'] = '*';
+    		res.headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Range';
+    		if (server.options.corsHeaders) {
+      			server.options.corsHeaders.split(',')
+          			.forEach((header) => { 
+          				res.headers['Access-Control-Allow-Headers'] += ', ' + header; });
+    		}
+  		}
+
+		getStat(fullPath).then((stat) => {			
+			if (stat.isDirectory()) {
+				sendFile(path.join(fullPath, 'index.html'));
+			} else {
+				sendFile(fullPath, stat, res, server);
+			}
+		}).catch((err) => {
+			if (err.code = 'ENOENT') {
+				sendError(res, server);
+			}
+		})
+
+  		/*if (server.options.cache) {
+			res.headers['Cache-Control'] = server.options.cacheHeaders;
+  		}*/
+
+		res.writeHead(200, res.headers);
+		
+		res.write('hello');
+		res.end();
+	} 
+}
+
+function sendFile() {
+	res.headers['Etag']           = JSON.stringify([stat.ino, stat.size, stat.mtime.getTime()].join('-'));
+    res.headers['Date']           = new Date().toUTCString();
+    res.headers['Last-Modified']  = new Date(stat.mtime).toUTCString();
+}
+
+// houston
+//static-server: requestHandler -> getFileStats -> sendFile(stat)
+function getStat(path) {
+	return new Promise((resolve, reject) => {
+		require('fs').stat(path, (err, stat) => {
+			if (err) return reject(err)
+			resolve(stat);
+		});
+	});
+}
+
+function sendFile(fullPath, stat, res, serv) {
+	if (serv.options.cache) {
+
+	}
+}
+
+new Server({cache: true, cors: true, name: 'dischain-serv'}).start();
