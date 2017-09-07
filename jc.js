aade647440d2dc33df481485f2d7b7c793602887
Aleksandr Shaytanov <rdischain@yandex.ru>
2017-09-07 19:48:25 +0400

    Update jc.js

--- jc.js
+++ jc.js
@@ -1,76 +1,135 @@
-// /routes/issue.js
+'use strict';
 
-const Issues = require('./models/issue.js')
+const // Home = require('./pages/home.jsx')
+    , Login = require('./pages/login.jsx')
+    , Register = require('./pages/register.jsx')
+    , Issue = require('./pages/issue.jsx')
+    , IssueEdit = require('./pages/issue_edit.jsx')
+    , Issues = require('./pages/issues.jsx')
+    , User = require('./pages/user.jsx')
+    , NotFound = require('./pages/not_found.jsx')
+    , Layout = require('./layout.js');
 
-/**
- * Get all issues
- */
-route.get('/issues', (req, res) => {
-    Issues.findAll()
-    .then((data) => {
-        res.json(data); res.end();
-    })
-    .catch((err) => {
-       res.status(500); res.end();
-    });
+const reactRouter = require('react-router')
+    , Router = reactRouter.Router
+    , Route = reactRouter.Router
+    , IndexRoute = reactRouter.IndexRoute
+    , browserHistory = reactRouter.browserHistory;
+
+const Routes = (
+    <Router history={browserHistory}>
+        <Route path='/' component={Layout}>
+            <Route path='register' component={Register}/>
+            <Route path='login' component={Login}/>
+            <Route path='issues' component={Issues}>
+                <Route path='issues/:issueId' component={Issue}>
+                    <Route path='issues/:issueId/edit' component={IssueEdit}/>
+                </Route>
+            </Route>
+            <Route path='users/:userId' component={User}/>
+        </Route>
+    </Router>
+);
+
+'use strict';
+
+const React = require('react');
+
+const AuthAction = require('./actions/auth.js');
+
+const Layout = React.createClass({
+    componentDidMount() {
+        AuthAction.fetchUser();
+    }
+
+    render() {
+        return(
+            <div>
+                {
+                    React.cloneElement(this.props.children, this.state)
+                }
+            </div>
+        );
+    }
 });
 
-/**
- * Get issues filtered by `status` param
- */
-route.get('/issues/:status', (req, res) => {
-    const filter = req.body;
+module.exports = Layout;
 
-    Issues.filterByStatus(filter)
-    .then((data) => {
-        res.json(data); res.end();        
-    })
-    .catch((err) => {
-        res.status(500); res.json({ msg: err.message }); res.end();
-    });
-});
+/*          Description
+**********************************************************/
+import ProductActions from "./actions/products"
+// const ProductActions = {
+//   FetchProducts: Reflux.createAction("FetchProducts")
+// };
+import ProductStore from "./stores/products"
+// import ProductActions from '../actions/products';
 
-/**
- * Post new issue. Issue id should be autoincrementable.
- * After successfull posting should redirect to `/issue`
- */
-route.post('/issues', (req, res) => {
-    const issueData = req.body;
+// const ProductStore = Reflux.createStore({
 
-    Issues.create(issueData)
-    .then(() => {
-        res.status(201); res.end();
-    })
-    .catch((err) => {
-        res.status(500); res.json({ msg: err.message }); res.end();
-    });
-});
+//   init() {
+//     this.listenTo(
+//       ProductActions.FetchProducts,
+//       this.onFetchProducts
+//     );
+//   },
 
-/** 
- * Update existing issue. After successfull updating should
- * redirect to `/issue`
- */
-route.put('/issues/:issue_id', (req, res) => {
-    const issueId = req.body.id;
-    const updateData = req.body.updateData;
-    
-    Issues.update(issueId, updateData)
-    .then(() => {
-        res.status(201); res.end();
-    })
-    .catch((err) => {
-        res.status(500); res.json({ msg: err.message }); res.end();
-    });
-});
+//   onFetchProducts(){
+//     Request
+//     .get('/products.json')
+//     .end((err, res)=>{
+//       if(err)
+//         alert(err)
+//       this.trigger(
+//         JSON.parse(res.text)
+//       );
+//     });
+//   }
 
-// /models/issue.js
-function update(issueId, updateData) {
-    return IssueModel.findOne({ issueId: issueId })
-    .then((issue) => {
-        Object.keys(issue).forEach((key) => {
-           updateData[key] && (issue[key] = updateData[key]);
-        });
+// });
 
-        return issue.save();
-    })
+const Layout = React.createClass({
+  mixins: [ Reflux.listenTo(ProductStore, 'onFetchProducts') ],
+
+  onFetchProducts(data) { this.setState({ products: data }) },
+
+  componentDidMount() {
+    ProductActions.FetchProducts();
+  },
+
+  render() {
+      return(
+        { React.cloneElement(this.props.children, this.state) }
+      );
+  }
 }
+
+LoginAction = {
+    Login: Reflux.createAction('Login');
+}
+
+LoginStore = Reflux.createStore({
+    init() {
+        this.listenTo(LoginAction.Login, 'onLogin')
+    },
+
+    onLogin(userData) {
+        fetch.post('/login', userData)
+        .then((err, res) => {
+            if (res.status != 401){
+                this.trigger(res.body);
+            }else {
+                // handle unauthorized   
+            }
+        });
+    }
+});
+
+Layout = React.createClass({
+    mixins: [
+        Reflux.listenTo(LoginStore, 'onLogin')
+    ],
+
+    onLogin(userData) {
+        this.setState({curUser: userData})
+    }
+})
