8a0149d365d0063bf2c433331e02e3c784690ce6
Aleksandr Shaytanov <rdischain@yandex.ru>
2017-10-04 18:58:29 +0400

    Update jc.js

--- jc.js
+++ jc.js
@@ -1,131 +1,66 @@
-const mysql = require('mysql');
-const config = require('../config');
-const constants = reqire('../constants');
-const tablesSchemas = require('./schemas.js');
+  'CREATE TABLE IF NOT EXISTS users ('
++ 'id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, '
++ 'name VARCHAR(20) NO NULL, '
++ 'email VARCHAR(30) NOT NULL, '
++ 'password VARCHAR NOT NULL, '
++ 'avatar BLOB, '
++ 'created_at DATE CURRENT_DATE NOT NULL, '
++ 'UNIQUE INDEX (email) '
++ 'INDEX (password)'
++ ');';
 
-// constants = {
-//     MODE_PRODUCTION: 'MODE_PRODUCTION',
-//     MODE_DEV: 'MODE_DEV',
-//     READ: 'READ*',
-//     WRITE: 'WRITE'
-// };
+  'CREATE TABLE IF NOT EXISTS votations ('
++ 'id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, '
++ 'title TEXT NOT NULL, '
++ 'description TEXT NOT NULL, '
++ 'creator_id INT NOT NULL, '
++ 'created_at DATE CURRENT_DATE NOT NULL, '
++ 'FULLTEXT (title, description), '
++ 'FOREIGN KEY (creator_id) REFERENCES users(id) ON DELETE CASCADE'
++ 'INDEX (creator_id)'
++ ');';
 
-let state = {
-  pool: null,
-  mode: null,
+  'CREATE TABLE IF NOT EXISTS votes ('
++ 'id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, '
++ 'votation_id INT NOT NULL, ' //constraint
++ 'creator_id INT NOT NULL, '
++ 'value ENUM ("1/2", "1", "2", "5", "8", "13", "20", "40", "100", "?", "inf"), '
++ 'INDEX (votation_id, creator_id)'
++ 'FOREIGN KEY (votation_id) REFERENCES users(id) ON DELETE CASCADE'
++ ');';
+
+// models/users.js
+function createUser(userData) {
+  let INSERT_USER = 'INSERT name, email, password, avatar INTO users VALUES ()'
+  db.query()
 }
 
-exports.init = function(mode) {
-  return new Promise((resolve, reject) => {
-    if (mode === constants.MODE_PRODUCTION) {
-      state.pool = mysql.createPoolCluster();
+function getAll() {}
 
-      state.pool.add('WRITE', {
-        host: '192.168.0.5',
-        user: 'your_user',
-        password: 'some_secret',
-        database: config.PRODUCTION_DB
-      })
+function getAllWithLimit(first, last) {}
 
-      state.pool.add('READ1', {
-        host: '192.168.0.6',
-        user: 'your_user',
-        password: 'some_secret',
-        database: config.PRODUCTION_DB
-      })
+function getUser(id) {}
 
-      state.pool.add('READ2', {
-        host: '192.168.0.7',
-        user: 'your_user',
-        password: 'some_secret',
-        database: config.PRODUCTION_DB
-      })
-    } else {
-      state.pool = mysql.createPool({
-        host: 'localhost',
-        user: 'your_user',
-        password: 'some_secret',
-        database: config.TEST_DB
-      })
-    }
+function deleteUser(id) {}
 
-    state.mode = mode
-    
-    if (!state.pool) return reject(new Error('Missing database connection!'))
-    
-    Promise.all(tablesSchemas.map((schema) => {
-      return _createTable(schema);
-    })).then(() => resolve());
-  });
-}
+function dropTable(){}
 
-exports.getConnection = function(type) {
-  const pool = state.pool
+// models/votations.js
+function createVotation(votationData) {}
 
-  return new Promise((resolve, reject) => {
-    if (type === constants.WRITE) {
-      pool.getConnection(constants.WRITE, (err, connection) => {
-        if (err) return reject(err);
+function getAll() {}
 
-        resolve(connection);
-      });
-    } else if (type === constants.READ) {
-      pool.getConnection(constants.READ, (err, connection) => {
-        if (err) return reject(err);
-        resolve(connection);
-      });
-    } else {
-      pool.getConnection((err, connection) => {
-        if (err) return reject(err);
-        resolve(connection);
-      });
-    }
-  });
-}
+function getAllWithLimit(first, last) {}
 
-exports.clear = function(tables, done) {
-  if (Array.isArray(tables)) {
-    return Promise.all(tables.map((table) => {
-      return _clearTable(table);
-    }));
-  } else {
-    return _clearTable(tables);
-  }
-}
+function getVotation(id) { /*with votes*/}
 
-/*                      Helper functions
-*******************************************************************/
+function getUserVotations() {}
 
-function _createTable(query) {
-  const pool = state.pool;
+function findVotation(text) {}
 
-  return new Promise((resolve, reject) => {
-    pool.query(query, (err) => {
-      if (err) return reject(err);
-      resolve();
-    });
-  })
-}
+function deleteVotation(id) {}
 
-function _clearTable(table) {
-  const pool = state.pool;
-  
-  return new Promise((resolve, reject) => {
-    pool.query('DELETE * FROM ' + table, (err) => {
-      if (err) return reject(err);
-      resolve();
-    });
-  });
-}
+function dropTable() {}
 
-//app.js:
-db.init(config.MODE_PRODUCTION)
-  .then(() => {
-    app.listen(3000, function() {
-      console.log('Listening on port 3000...')
-    });
-  })
-  .catch((err) => {
-    console.log(err)
-    process.exit(1)
-  });
+// models/votes.js
+function createVote(voteData) {}
