a10fcb6fa9800b5f4dd4f8287c3840a19febf8be
Aleksandr Shaytanov <rdischain@yandex.ru>
2017-10-26 18:31:24 +0400

    Update layout.js

--- layout.js
+++ layout.js
@@ -1,112 +1,183 @@
-// создать отдельные редьюсеры для authReducer, usersReducer, votationsReducer, currentVotationReducer
-// и для каждого из них прописать initialState
+ /*                     Votes - Votations
+  ***************************************************************/
+    storeVote: function(voteData) {
+      const key = 'vote:' + voteData.creator_id; // <---------
+      return new Promise((resolve, reject) => {
+        con.hmset(key, voteData, (err) => {
+          if (err) return reject(err);
+          return resolve();
+        });
+      });      
+    },
 
-　
-　
-// reducers/loginReducer.js
-import { 
-  SET_AUTH, 
-  CHANGE_LOGIN_FORM, 
-  SENDING_LOGIN_REQUEST,
-  SET_LOGIN_ERROR_MESSAGE
-} from '../constants/LoginConstants.js';
+    getVote: function(creator_id) {
+      const key = 'vote:' + creator_id;
+      return new Promise((resolve, reject) => {
+        con.hgetall(key, (err, res) => {
+          if (err) return reject(err);
+          return resolve(res);
+        });
+      });  
+    },
 
-import auth from '../utils/auth';
-
-const assign = Object.assign;
-
-const initialState = {
-  loginFormState: {
-    email: '',
-    password: ''
-  },
-  sendingLoginRequest: false,
-  loginFormErrorMessage: '',
-  loggedIn: auth.loggedIn()
-};
-
-export function loginReducer(state = initialState, action) {
-  switch(action.type) {
-    case SET_AUTH: 
-      return assi({}, state, {
-        loggedIn: action.newState
+    removeVote: function(creator_id, fields) {
+      const key = 'vote:' + creator_id;
+      return new Promise((resolve, reject) => {
+        con.hdel(key, fields, (err, numDeleted) => {
+          if (err) return reject(err);
+          return resolve(numDeleted);
+        });
       });
-      break;
-    case CHANGE_LOGIN_FORM:
-      return assign({}, state, {
-        loginFormState: action.newState
+    },
+
+    storeVoteByVotation: function(votationId, voteId) {
+      const key = 'votation:' + votationId + ':votes';
+      return new Promise((resolve, reject) => {        
+        con.sadd(key, voteId, (err, res) => {
+          if (err) return reject(err);
+          return resolve(res);
+        });
       });
-      break;
-    case SENDING_LOGIN_REQUEST:
-      return assign({}, state, {
-        sendingLoginRequest: action.newState
+    },    
+
+    getVotesByVotation: function(votationId) {
+      const key = 'votation:' + votationId + ':votes';
+      return new Promise((resolve, reject) => {
+        con.smembers(key, (err, res) => {
+          if (err) return reject(err);
+          
+          let votes = [];
+
+          res.reduce((init, creator_id) => {
+            return init.then(() => {
+              return this.getVote(creator_id).then((res) => {
+                votes.push(res);
+                return Promise.resolve();              
+              });
+            })            
+          }, Promise.resolve())
+          .then(() => {
+            return resolve(votes);
+          })          
+        });
       });
-      break;
-    case SET_LOGIN_ERROR_MESSAGE:
-      return assign({}, state, {
-        loginFormErrorMessage: action.newState
+    },   
+    
+    removeAllVotesByVotation: function(votationId) {
+      const key = 'votation:' + votationId + ':votes';
+      return new Promise((resolve, reject) => {
+        con.smembers(key, (err, res) => {
+          if (err) return reject(err);
+          
+          res.reduce((init, creator_id) => {
+            return init.then(() => this.removeVote(creator_id, Object.keys({ value, creator_id, votation_id})))            
+          }, Promise.resolve())
+          .then(() => {
+            return resolve(votes);
+          })          
+        });
       });
-      break;
-    default:
-      return state;
-  }
-}
+    },
 
-// reducers/AuthReducer.js
-import { 
-  CHANGE_REGISTER_FORM, 
-  SENDING_REGISTER_REQUEST,
-  SET_REGISTER_ERROR_MESSAGE
-} from '../constants/RegisterConstants.js';
-
-import auth from '../utils/auth';
-
-const assign = Object.assign;
-
-const initialState = {
-  registerFormState: {
-    name: '',
-    email: '',
-    password1; '',
-    password2: '',
-    avatar: ''
-  },
-  sendingRegisterRequest: false,
-  registerFormErrorMessage: ''
-};
-
-export function registerReducer(state = initialState, action) {
-  switch(action.type) {
-    case CHANGE_REGISTER_FORM:
-      return assign({}, state, {
-        registerFormState: action.newState
+    removeVoteByVotation: function(votationId, voteId) {
+      const key = 'votation:' + votationId + ':votes';
+      return new Promise((resolve, reject) => {
+        con.srem(key, voteId, (err, numRemoved) => {
+          if (err) return reject(err);
+          return resolve(numRemoved);
+        });
       });
-      break;
-    case SENDING_REGISTER_REQUEST:
-      return assign({}, state, {
-        sendingRegisterRequest: action.newState
+    }
+/*************************************************************************/
+
+      // как отправить сообщение конкретному сокету?
+      // vote должен храниться по id создателя, а не голосования, т.к. своего айди покa не имеет
+      socket.on('send_vote', (voteData) => {
+        // сохранить value в редис до завершения голосования
+        cache.storeVote(voteData)
+        // привяжем голос к голосованию
+        .then(() => cache.storeVoteByVotation(vote.votationId, vote.creatorId))
+        // посылает окончательный результат голосования на creatorId (можно отпр. только раз)
+        .then(() => socket.to(voteData.votationId).emit('ADD_VOTE', voteData));
       });
-      break;
-    case SET_REGISTER_ERROR_MESSAGE:
-      return assign({}, state, {
-        registerFormErrorMessage: action.newState
-      });
-      break;
-    default:
-      return state;
-  }
-}
-
-// actions/loginActions.js
-import { 
-  SET_AUTH, 
-  CHANGE_LOGIN_FORM, 
-  SENDING_LOGIN_REQUEST,
-  SET_LOGIN_ERROR_MESSAGE
-} from '../constants/LoginConstants.js';
-
-import auth from '../utils/auth';
-
-export function login(email, password) {
   
-}
+      socket.on('save_votation', (votationData) => {
+        // вытаскивает временные значения голосов из redis
+        cache.getVotesByVotation(votationData.id)
+        .then((votes) => {
+        // и сохраняем готовое голосование с результатами в бд
+          return votesData.reduce((initial, vote) => {
+            vote.votation_id = votationData.id;
+            return initial.then(() => votes.query(votesConstants.CREATE_VOTE, vote));
+          }, Promise.resolve());
+        })
+        // затираем кеш
+        .then(() => cache.removeVotation(votationData.id, Object.keys(votationData))
+        .then(() => cache.removeAllVotesByVotation(votationData.id)) // дописать
+        .then(() => cache.removeAllUserFromVotation(votationData.id)) // дописать
+        .then(() => {
+        // закрываем комнату
+          socket.broadcast.to(votationId).emit('CLOSE_VOTATION');
+        })
+        .catch((err) => {          
+          votations.query(votationsConstants.DELETE_VOTATION, votationData.id)
+          .then(() => socket.emit('VOTATION_SAVING_ERROR'))          
+        })
+      });
+    });
+
+// после успешного создания комнаты клиент должен перейти в соотв. пространство имен
+
+describe('votationRoom namespace', () => {
+    const 
+    votation1 = {
+      title: 'test title',
+      description: 'test desc',
+      creator_id: '1'
+    },
+    user1 = {
+      name: 'user1',
+      id: '1'
+    }, 
+    user2 = {
+      name: 'user2',
+      id: '2'
+    };
+    let client1, client2, votationId;
+    // before:
+    // 1. Создать объект голосования и поместить его в редис
+    // 2. Создать объект юзера
+    // socket.emit('join', roomId);
+    before((done) => {
+      client1 = io.connect(
+        'http://localhost:3001/votations',
+        { transports: ['websocket'] });
+      client1.on('connect', () => {
+        client1.emit('CREATE_VOTATION', votation1);
+
+        client1.on('VOTATION_CREATED', (id) => {
+          votationId = id;          
+          done();
+        });
+     });
+    });
+
+    after(() => {
+      flush().then(done);
+    });
+
+    it('should update list of participants after joininig votation room', (done) => {       
+      client1 = io.connect(
+        'http://localhost:3001/votationRoom',
+        { transports: ['websocket'] }),
+
+      client1.on('connect', () => {
+        client1.on('UPDATE_PARTICIPANTS', (users) => {
+          console.log(users);
+          done();
+        });
+        
+        client1.emit('join', votationId);
+      });
+    });
+  });
