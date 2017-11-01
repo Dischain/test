4846a6e316d8e5666c6b5e5b91e1c3fd17512b67
Aleksandr Shaytanov <rdischain@yandex.ru>
2017-11-01 19:16:08 +0400

    Update user.js

--- user.js
+++ user.js
@@ -1,251 +1,239 @@
-'use strict';
+// ./ws/votationSocket.js
+import SocketClient from './SocketClient.js';
 
-const chai = require('chai')
-    , io = require('socket.io-client')
-    , expect = chai.expect
-    , flush = require('../../app/cache').flush
-    , cache = require('../../app/cache').votationCache
+let socket = new SocketClient('/votations', );
 
-    , modelNames = require('../../../app/model').modelNames
-    , votations = require('../../../app/model').getModel(modelNames.VOTATIONS_MODEL)
-    , votationConstants = require('../../../app/model/votations').constants;
-
-describe('websocket server', () => {
-  describe('votations namespace', () => {
-    const votation1 = {
-      title: 'test title',
-      description: 'test desc',
-      creator_id: '1'
-    };
-
-    it('should create votation', (done) => {
-      const client1 = io.connect(
-        'http://localhost:3001/votations',
-        { transports: ['websocket'] });
-
-      client1.on('connect', () => {
-        client1.emit('CREATE_VOTATION', votation1);
-
-        client1.on('VOTATION_CREATED', (votationId) => {
-          expect(votationId).to.be.a('number');
-          done();
-        });
-      });
-    });
-  });
-
-  describe('votationRoom namespace', () => {
-    const 
-    votation1 = {
-      title: 'test title',
-      description: 'test desc',
-      creator_id: '1'
-    },
-    user1 = {
-      name: 'user1',
-      id: '1'
-    }, 
-    user2 = {
-      name: 'user2',
-      id: '2'
-    };
-    let client1, client2, votationId;
-
-    before((done) => {
-      client1 = io.connect(
-        'http://localhost:3001/votations',
-        { transports: ['websocket'] });
-      client1.on('connect', () => {
-        client1.emit('CREATE_VOTATION', votation1);
-
-        client1.on('VOTATION_CREATED', (id) => {
-          votationId = id;
-          done();
-        });
-     });
-    });
-
-    after((done) => {
-      flush().then(done);
-    });
-
-    it('should UPDATE_PARTICIPANTS after joininig votation '
-    + 'room', (done) => {       
-      client1 = io.connect(
-        'http://localhost:3001/votationRoom',
-        { 
-          transports: ['websocket'],
-          'force new connection': true
-        });
-
-      client2 = io.connect(
-        'http://localhost:3001/votationRoom',
-        { 
-          transports: ['websocket'],
-          'force new connection': true 
-        });
-
-      client1.on('connect', () => {
-        client1.emit('join', { votationId, userId: user1.id });
-
-        client2.on('connect', () => {
-          client2.emit('join', { votationId, userId: user2.id });
-        });           
-      });
-      client1.on('UPDATE_PARTICIPANTS', (users) => {
-        expect(users.length).to.be.equal(2);
-        client1.disconnect();
-        client2.disconnect();
-        done();
-      });
-    });
-
-    // 1. попробовать клиент2 подсоединить к комнате namespace`у votationRoom
-    // 2. добавить force new connection
-    // 3. не забывать выполнять дисконнект
-    it('should invite user', (done) => {
-      client1 = io.connect(
-        'http://localhost:3001/votationRoom',
-        { 
-          transports: ['websocket'],
-          'force new connection': true 
-        });
-
-      
-      client2 = io.connect(
-        'http://localhost:3001/',
-        { 
-          transports: ['websocket'],
-          'force new connection': true 
-        });
-
-      client1.on('connect', () => {
-        client1.emit('join', { votationId, userId: user1.id });
-        client1.emit('invite', { 
-          creatorId: user1.id,
-          votationId,
-          title: votation1.title,
-          description: votation1.description,
-          users: [2]
-        });
-
-      });
-      client2.on('invite', (data) => {
-        console.log(data);
-        client1.disconnect();
-        client2.disconnect();
-        done();
-      })
-    });
-
-    it('should trigger REMOVE_USER after disconnect', (done) => {
-      client1 = io.connect(
-        'http://localhost:3001/votationRoom',
-        { 
-          transports: ['websocket'],
-          'force new connection': true 
-        });
-
-      client2 = io.connect(
-        'http://localhost:3001/votationRoom',
-        { 
-          transports: ['websocket'],
-          'force new connection': true 
-        });
-
-      client1.on('connect', () => {
-        client1.emit('join', { votationId, userId: user1.id });
-
-        client2.on('connect', () => {
-          client2.emit('join', { votationId, userId: user2.id });
-          client2.disconnect();
-        });           
-      });
-      client1.on('REMOVE_USER', (userId) => {
-        console.log(userId);
-        expect(userId).to.be.equal(user2.id);
-        client1.disconnect();
-        done();
-      });
-    });
-
-    it('should send vote to votation creator', (done) => {
-      client1 = io.connect(
-        'http://localhost:3001/votationRoom',
-        { 
-          transports: ['websocket'],
-          'force new connection': true 
-        });
-
-      client2 = io.connect(
-        'http://localhost:3001/votationRoom',
-        { 
-          transports: ['websocket'],
-          'force new connection': true 
-        });
-
-      client1.on('connect', () => {
-        client1.emit('join', { votationId, userId: user1.id });
-
-        client2.on('connect', () => {
-          client2.emit('join', { votationId, userId: user2.id });
-          client2.emit('send_vote', { value: '1', creatorId: '2', votationId});          
-        });
-      });
-
-      client1.on('ADD_VOTE', (voteData) => {
-        console.log(voteData);
-        client1.disconnect();
-        client2.disconnect();
-        done();
-      });
-    });
-  });
-
-  describe('save votation', () => {
-    const     
-    vote1 = { value: '1', creatorId: '1' },
-    vote2 = { value: '1', creatorId: '2' };
-    let
-    client1, client1, 
-    votation1 = { title: 'test title', description: 'test desc', creator_id: '1' },
-    votationId;
-
-    before((done) => {
-      flush()
-      .then(() => votations.query(votationConstants.CREATE_TABLE))
-      .then(() => cache.storeVote(vote1))
-      .then(() => cache.storeVote(vote2))
-      .then(() => votations.query(votationConstants.CREATE_VOTATION, votation1))
-      .then(({ insertId }) => { votationId = insertId; votation1.id = insertId; return Promise.resolve(); })
-      .then(() => cache.storeVoteByVotation(votationId, vote1.creatorId))
-      .then(() => cache.storeVoteByVotation(votationId, vote2.creatorId))
-      .then(() => {
-        client1 = io.connect(
-          'http://localhost:3001/votationsRoom',
-          { 
-            transports: ['websocket'],
-            'force new connection': true
-          });
-        client2 = io.connect(
-          'http://localhost:3001/votationsRoom',
-          { 
-            transports: ['websocket'],
-            'force new connection': true
-          });
-        
-        client1.on('connect', () => {
-          client1.emit('join', { votationId, userId: user1.id });
-          client1.emit('save_votation', votation1);
-        });
-      });
-    });
-
-    it('should save votation', (done) => {
-      client1.on('CLOSE_VOTATION', (success) => {
-        expect(success).to.be.equal(true);
-        done();
-      }); // сервер должен передать true (сейчас он ничего не передает)
-    });
-  });
+socket.on('VOTATION_CREATED', (votationId) => {
+  socket.dispatch(setNewlyCreatedVotationId(votationId));
 });
+
+// ./actions/socketVotationActions.js
+import { 
+  CREATE_VOTATION, 
+  SET_CURRENT_VOTATION_ID 
+} from '../constants/socketVotationConstants.js';
+
+let useSocket = true, nsp = 'votations';
+
+export function createVotation(votationData) {
+  return {
+    useSocket,
+    nsp, 
+    type: CREATE_VOTATION, 
+    promise: (socket) => socket.emit(CREATE_VOTATION, votationData)   
+  };
+}
+
+export function setNewlyCreatedVotationId(votationId) {
+  return {
+    type: SET_CURRENT_VOTATION_ID,
+    votationId
+  };
+}
+
+// .reducers/socketVotationReducer.js <-------------------new
+const initialState = {
+  currentVotationId: '',
+  currentVotationTitle: '',
+  currentVotationDescription: ''
+}
+
+export default function socketVotationReducer(state = initialState, action) {
+  switch(action.type) {
+    case SET_CURRENT_VOTATION_ID:
+      return assign({}, state, {
+        currentVotationId: action.votationId
+      });
+    default:
+      return state;
+  }
+}
+
+// .ws/votationRoomSocket.js <-------------new
+import SocketClient from './SocketClient.js';
+
+let socket = new SocketClient('/votationRoom');
+
+socket.on('UPDATE_PARTICIPANTS', (users) => {
+  socket.dispatch(updateParticipants(users));
+});
+
+socket.on('INVITE_USER', (data) => {
+  socket.dispatch(enterVotation(data));
+});
+
+socket.on('REMOVE_USER', (userId) => {
+  socket.dispatch(removeUser(userId));
+});
+
+socket.on('ADD_VOTE', (data) => {
+  socket.dispatch(addVote(data));
+});
+
+socket.on('CLOSE_VOTATION', (id) => {
+  socket.dispatch(closeVotation(id));
+});
+
+// ./actions/socketVotationRoomActions.js <-----------------new
+import {
+  JOIN, 
+  INVITE, 
+  SEND_VOTE, 
+  SAVE_VOTATION,
+
+  UPDATE_PARTICIPANTS, 
+  ADD_USER,
+  REMOVE_USER,
+  ADD_VOTE,
+  CLOSE_VOTATION
+} from '../constants/socketVotationRoomConstants.js';
+
+let useSocket = true, nsp = 'votationRoom';
+
+// { votationId, userId, cretorId }
+export function joinVotation(votationData) {
+  return {
+    useSocket,
+    nsp, 
+    type: JOIN, 
+    promise: (socket) => socket.emit(JOIN, votationData)   
+  };
+}
+
+export function inviteToVotation(data) {
+  return {
+    useSocket,
+    nsp, 
+    type: INVITE, 
+    promise: (socket) => socket.emit(INVITE, votationData)   
+  };
+}
+
+export function sendVote(data) {
+  return {
+    useSocket,
+    nsp, 
+    type: SEND_VOTE, 
+    promise: (socket) => socket.emit(SEND_VOTE, votationData)   
+  };
+}
+
+export function saveVotation(data) {
+  return {
+    useSocket,
+    nsp, 
+    type: SAVE_VOTATION, 
+    promise: (socket) => socket.emit(SAVE_VOTATION, votationData)   
+  };
+}
+
+export function updateParticipants(users) {
+  return { type: UPDATE_PARTICIPANTS, users };
+}
+
+export function enterVotation(data) {
+  return { type: ADD_USER, data };
+}
+
+export function removeUser(userId) {
+  return { type: REMOVE_USER, userId };
+}
+
+export function addVote(data) {
+  return { type: ADD_VOTE, data };
+}
+
+export function closeVotation(id) {
+  return {
+    type: CLOSE_VOTATION, id };
+}
+
+// .reducers/socketVotationRoomReducer.js <-------------------new
+const initialState = {
+  currentVotationParticipants: [],
+  currentVotationVotes: [],
+  updateVotationList: false,
+
+  currentInviteData: {
+    creatorId: '',
+    title: '',
+    description: ''
+  }
+}
+
+export default function socketVotationReducer(state = initialState, action) {
+  switch(action.type) {
+    case UPDATE_PARTICIPANTS:
+      return assign({}, state, {
+        currentVotationParticipants: action.users
+      });
+    case ADD_USER: {
+      let temp = state.currentVotationParticipants;
+      let { user, votationId, ...restInviteData } = action.data;
+      temp.push(user);
+
+      return assign({}, state, {
+        currentVotationParticipants:  temp,
+        currentInviteData: restInviteData
+      });
+    }
+    case REMOVE_USER: {
+      let temp = state.currentVotationParticipants;
+      let indexToRemove = temp.reduce((initial, id, index) => {
+        if (id === action.userId) return initial = index;
+	    else return initial;
+      }, null);
+      temp.splice(indexToRemove, 1);
+
+      return assign({}, srtate, {
+          currentVotationParticipants: temp;
+      });
+    }
+    case ADD_VOTE: {
+      let temp = state.currentVotationVotes;
+      
+      temp.push(action.voteData);
+
+      return assign({}, state, {
+        currentVotationVotes: temp
+      });
+    }
+    case CLOSE_VOTATION: {
+        return assign({}, state, {
+          updateVotationList: true,
+          currentVotationId: '',
+          currentVotationParticipants: [],
+          currentVotationVotes: [],  
+        });
+    }
+    default:
+      return state;
+  }
+}
+
+// ./middleware/ws.js
+export default function socketMiddleware(sockets) {
+  return ({ dispatch, getState }) => next => action => {
+    if (typeof action === 'function') {
+      return action(dispatch, getState);
+    }
+
+    const { useSocket, nsp, type, promise, ...rest } = action;
+
+    let socket = sockets[nsp];
+
+    if (!useSocket) {
+      return next(action);
+    }
+
+    if (!socket.dispatch) socket.dispatch = dispatch;
+
+    return promise(socket)
+      .then((result) => {
+        return next({ ...rest, result, type }) // тут включается редьюсер и изменяет состояние
+                                               // в соотв. с указанным типом.
+      });
+  }
+}
+
+　
