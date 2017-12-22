f182b44a3b6e505f03eaa43795428226b8d5f270
Aleksandr Shaytanov <rdischain@yandex.ru>
2017-12-22 18:31:29 +0400

    Update parallel.js

--- parallel.js
+++ parallel.js
@@ -1,155 +1,82 @@
-<head>
-  <style>
-    body, form, ul {
-      margin: 0px;
-    }
-    ul { padding: 0px; }
-    li { text-decoration: none; }
-    a {
-      /*color: #0366d6;*/
-      text-decoration: none;
-      background-color: transparent;
-    }
-    .fixed-left {
-      position: fixed;
-      top: 24px;      
-    }
-    .clearfix:after {
-      display: block;
-      clear: both;
-      content: "";
-    }
-    .float-left {
-      float: left !important;
-    }
-    .float-right {
-      float: right !important;
-    }
-    .position-relative {
-      position: relative !important;
-    }
-    .text-right {
-      text-align: right !important;
-    }
-    .display-table {
-      display: table !important;
-    }
-    .display-inline {
-      display: inline !important;
-    }
-    .display-block {
-      display: block !important;
-    }
-    .text-gray {
-      color: #586069 !important;
-    }
-    .container {
-      max-width: 1012px;
-      margin-top: 24px;
-      margin-left: auto;
-      margin-right: auto;
-      box-sizing: border-box;
-    }
-    .content {
-      padding-left: 8px;
-      width: 100%;
-      box-sizing: border-box;
-    }    
-    
-    /* ./_user-card.css */
-    .user-card-xsm {
-      padding-top: 8px;
-      padding-bottom: 8px;
-      width: 100%;
-      border-bottom: 1px #e1e4e8 solid;   
-    }
-    .user-card-xsm__avatar {}
-    .user-card-xsm__name {}
-    /* ./_buttons.css */
-    .btn-rm {}
+final class Dict [A, B] {
+  
+}
 
-    /* ./_usr-selector.css */
-    .usr-selector {      
-      
-    }
-    .usr-selector__selector { 
-      border: 1px #e1e4e8 solid;
-      border-radius: 3px;
-      margin: 10px 10px 5px 10px;
-      max-height: 250px;
-    }
-    .usr-selector__item:hover {
-      background-color: #e1e4e8;
-    }
-    .usr-selector__selected {}
+final class DictHT [A, B] {
+  import DictHT._
 
-    /* ./_search-box.css */
-    .search-box-sm {
-      width: 100%;
-      min-height: 34px;
-      box-sizing: border-box;
-      border-radius: 2px;
-      font-size: 16px;
-      padding-left: 25px;
-      border: 1px #e1e4e8 solid;
-      background-color: #fff;
-      background-image: url('search-icon-sm.png');
-      background-repeat: no-repeat;
-      background-position: 5px 5px;
-      font-size: 14px;
+  private var table: Array[DictEntry[A, B]] = new Array(initialCapacity)
+
+  private var _loadFactor = defaultLoadFactor
+  // след. два значения нужны для resize: if (tableSize > threshold) -> resize(2 * table.length)
+  private var tableSize: Int = 0 // кол. элементов в таблице
+  private var threshold: Int = initialThreshold(_loadFactor) // след. значение, после кот. вып. resize
+
+  // нужен только для вычисления первичного размера табл., больше не используется.
+  private def initialSize: Int = 4
+  private def initialThreshold(_loadFactor: Int): Int = newThreshold(_loadFactor, initialCapacity)
+  private def initialCapacity: Int = capacity(initialSize)
+
+  private def index(hcode: Int): Int = hcode & sizeMask
+  private def sizeMask: Int = tableSize == 0 ? 0 : tableSize - 1 // изм. при resize
+}
+
+private object DictHT {
+  private final def defaultLoadFactor: Int = 750
+  private final def loadFactorDenum = 1000
+
+  private final def newThreshold(_loadFactor: Int, size: Int) = 
+    ((size.toLong * _loadFactor) / loadFactorDenum).toInt
+
+  private final def sizeForThreshold(_loadFactor: Int, thr: Int) = 
+    ((thr.toLong * loadFactorDenum) / _loadFactor).toInt
+
+  private final def capacity(expectedSize: Int) = expectedSize << 1  
+}
+
+final class DictEntry [A, B] (val key: A, var value: B) {
+  var next: DictEntry[A, B]
+}
+
+object MurmurHash2 {
+  def hashString(key: String): Int = {
+    val m = 0x5bd1e995
+    val seed = 0
+    val r = 24
+
+    var len = key.length
+    var h = seed ^ len
+  
+    var data = key
+    var k
+  
+    while (len >= 4) {
+      k  = data.charCodeAt(0)
+      k |= data.charCodeAt(1) << 8
+      k |= data.charCodeAt(2) << 16
+      k |= data.charCodeAt(3) << 24
+
+      k *= m
+      k ^= k >> r
+      k *= m
+
+      h *= m
+      h ^= k
+
+      data += 4
+      len -= 4
     }
 
-  </style>
-</head>
-<body>
-  <div class="container">
-  <div class="content">
-    <div class="usr-selector clearfix">
-      <div class="usr-selector__selector float-left">
-        <form>
-          <input class="search-box-sm" type="search" placeholder="Search users..." autocomplete="off"></input>          
-        </form>
-        <ul >
-          <li class= 'user-card-xsm usr-selector__item clearfix'>
-            <img class = 'user-card-xsm__avatar float-left' 
-              src = "./ava.png" width='45' height='45'></img>                  
-            <div class='user-card-xsm__name float-left'>
-              <a href="#">Dischain</a>
-            </div>
-          </li>
-          <li class= 'user-card-xsm usr-selector__item clearfix'>
-            <img class = 'user-card-xsm__avatar float-left' 
-              src = "./ava.png" width='45' height='45'></img>                  
-            <div class='user-card-xsm__name float-left'>
-              <a href="#">Dischain</a>
-            </div>
-          </li>
-          <li class= 'user-card-xsm usr-selector__item clearfix'>
-            <img class = 'user-card-xsm__avatar float-left' 
-              src = "./ava.png" width='45' height='45'></img>                  
-            <div class='user-card-xsm__name float-left'>
-              <a href="#">Dischain</a>
-            </div>
-          </li>
-          <li class= 'user-card-xsm usr-selector__item clearfix'>
-            <img class = 'user-card-xsm__avatar float-left' 
-              src = "./ava.png" width='45' height='45'></img>                  
-            <div class='user-card-xsm__name float-left'>
-              <a href="#">Dischain</a>
-            </div>
-          </li>
-        </ul>
-      </div>
-      <div class="usr-selector__selected float-left">
-        <div class="user-card-xsm clearfix">
-          <img class = 'user-card-xsm__avatar float-left' 
-            src = "./ava.png" width='45' height='45'></img>                  
-          <div class='user-card-xsm__name float-left'>
-            <a href="#">Dischain</a>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  </div>
-</body>
+    len match {
+      case 3 => h ^= data.charCodeAt(2) << 16
+      case 2 => h ^= data.charCodeAt(1) << 8
+      case 1 => h ^= data.charCodeAt(0) h *= m
+    }
+
+    h ^= h >> 13
+    h *= m
+    h ^= h >> 15
+
+    h
+  }
+}
