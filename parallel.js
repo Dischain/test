c8a559274452d1ad0a6c6e11acdee2eeb1af12d9
Aleksandr Shaytanov <rdischain@yandex.ru>
2017-12-25 19:15:29 +0400

    Update parallel.js

--- parallel.js
+++ parallel.js
@@ -1,82 +1,59 @@
-final class Dict [A, B] {
-  
+// в dictRehash рагумент n: Int = 1 назначить равным единице по умолчанию,
+// rehahsStep заменить на dictRehash
+
+// DictHT.clear переделать на:
+def clear: Unit = {
+  for (i <- 0 to size - 1 by -1) table(i) = null
+  used = 0
+}
+// так же set переименовать на put!
+
+class DictEntryBuilder[A, B]  {
+  private var storage: DictEntry[A, B] = _
+
+  def +=(e: DictEntry[A, B]) = {
+    if (storage == null)
+      storage = e
+    else 
+      storage.next = e
+
+    this 
+  }
+
+  def reset() = {
+    storage = initial
+    this
+  }
+
+  def result(): Option[DictEntry[A, B]] = Some(storage)
 }
 
-final class DictHT [A, B] {
-  import DictHT._
+// DictEntry
+def filter(f: DictEntry[A, B] => Boolean): Option[DictEntry[A, B]] = {
+  val builder = new DictEntryBuilder[A, B]
 
-  private var table: Array[DictEntry[A, B]] = new Array(initialCapacity)
+  def loop(acc: DictEntryBuilder[A, B], left: DictEntry[A, B], f: DictEntry[A, B] => Boolean) = {
+    if (f(left)) acc += left
+    loop(acc, left.next, f)
+  }
 
-  private var _loadFactor = defaultLoadFactor
-  // след. два значения нужны для resize: if (tableSize > threshold) -> resize(2 * table.length)
-  private var tableSize: Int = 0 // кол. элементов в таблице
-  private var threshold: Int = initialThreshold(_loadFactor) // след. значение, после кот. вып. resize
-
-  // нужен только для вычисления первичного размера табл., больше не используется.
-  private def initialSize: Int = 4
-  private def initialThreshold(_loadFactor: Int): Int = newThreshold(_loadFactor, initialCapacity)
-  private def initialCapacity: Int = capacity(initialSize)
-
-  private def index(hcode: Int): Int = hcode & sizeMask
-  private def sizeMask: Int = tableSize == 0 ? 0 : tableSize - 1 // изм. при resize
+  loop(builder, this, f).result
 }
 
-private object DictHT {
-  private final def defaultLoadFactor: Int = 750
-  private final def loadFactorDenum = 1000
+// DictHT
+def filterIndex(hash, key): Option[Int] ={
+  val idx = hash & table.sizeMask
+  var he = table.get(idx)
 
-  private final def newThreshold(_loadFactor: Int, size: Int) = 
-    ((size.toLong * _loadFactor) / loadFactorDenum).toInt
-
-  private final def sizeForThreshold(_loadFactor: Int, thr: Int) = 
-    ((thr.toLong * loadFactorDenum) / _loadFactor).toInt
-
-  private final def capacity(expectedSize: Int) = expectedSize << 1  
+  he.filter(e => e.key == key)      
 }
 
-final class DictEntry [A, B] (val key: A, var value: B) {
-  var next: DictEntry[A, B]
-}
+private def dictKeyIndex(key: A): Option[Int] = {
+  val hash = hashString(key)
 
-object MurmurHash2 {
-  def hashString(key: String): Int = {
-    val m = 0x5bd1e995
-    val seed = 0
-    val r = 24
-
-    var len = key.length
-    var h = seed ^ len
-  
-    var data = key
-    var k
-  
-    while (len >= 4) {
-      k  = data.charCodeAt(0)
-      k |= data.charCodeAt(1) << 8
-      k |= data.charCodeAt(2) << 16
-      k |= data.charCodeAt(3) << 24
-
-      k *= m
-      k ^= k >> r
-      k *= m
-
-      h *= m
-      h ^= k
-
-      data += 4
-      len -= 4
-    }
-
-    len match {
-      case 3 => h ^= data.charCodeAt(2) << 16
-      case 2 => h ^= data.charCodeAt(1) << 8
-      case 1 => h ^= data.charCodeAt(0) h *= m
-    }
-
-    h ^= h >> 13
-    h *= m
-    h ^= h >> 15
-
-    h
+  val res = for { table <- dict } yield table.filterIndex(hash, key)
+  res filter {
+    case Some(index) => Some(index)
+    case None =>
   }
 }
