99dac92d83050d2a56b335efa37455dd7de2c7e8
Aleksandr Shaytanov <rdischain@yandex.ru>
2018-01-18 16:07:30 +0000

    Update jc.js

--- jc.js
+++ jc.js
@@ -1,273 +1,225 @@
-import { 
-  getVotations, 
-  getUserVotations, 
-  searchVotations 
-} from '../actions/votationsActions.js';
-import { searchUsers } from '../actions/userActions.js';
-// import { setCurView, setPaginationOffset } from '../actions/dataListActions.js';
-import { 
-  DEFAULT_PAGINATION_OFFSET, 
-  DEFAULT_PAGINATION_LIMIT 
-} from '../constants/commonConstants.js';
-import {
-  DATA_LIST_VOTATIONS_VIEW,
-  DATA_LIST_USER_VOTATIONS_VIEW,
-  DATA_LIST_VOTATIONS_SEARCH_VIEW,
-  DATA_LIST_USERS_SEARCH_VIEW
-} from '../constants/dataListConstants.js'
-
-class DataList extends  Component {
-  componentDidMount() {
-    const { 
-      location, getVotations, getUserVotations,
-      setCurView, setPaginationOffset,
-      currentPaginationOffset
-    } = this.props;
-
-    if (location === '/dashboard') {
-      //setCurView(DATA_LIST_VOTATIONS_VIEW);      
-      getVotations({ 
-        offset: DEFAULT_PAGINATION_OFFSET, 
-        limit: DEFAULT_PAGINATION_LIMIT
-      });
-    } else if (/users\/[0-9]+/.test(location)) {
-      let userId = Number.parseInt(location.split('/')[2]);
-
-      //setCurView(DATA_LIST_USER_VOTATIONS_VIEW);
-      getUserVotations({
-        offset: DEFAULT_PAGINATION_OFFSET, 
-        limit: DEFAULT_PAGINATION_LIMIT, 
-        userId 
-      });
-    }
-
-    //setPaginationOffset(currentPaginationOffset + DEFAULT_PAGINATION_OFFSET);
+node match {
+  // case 1.1
+  case Tree(c, Tree(Black, ll, lk, lv, Leaf()), k, v, r)
+  if (implicitly[Ordering[B]].eq(lk, key)) => ll match {
+    case Tree(Red, Leaf(), ck, cv, Leaf()) => 
+      Tree(c, Tree(Black, Leaf(), ck, cv, Leaf()), k, v, r)
+  }
+  // case 1.2
+  case Tree(c, Tree(Black, Leaf(), lk, lv, lr), k, v, r)
+  if (implicitly[Ordering[B]].eq(lk, key)) => lr match {
+    case Tree(Red, Leaf(), ck, cv, Leaf()) => 
+      Tree(c, Tree(Black, Leaf(), ck, cv, Leaf()), k, v, r)
+  }
+  // case 1.3
+  case Tree(Tree(c, l, k, v, Tree(Black, rl, rk, rv, Leaf()))
+  if (implicitly[Ordering[B]].eq(rk, key)) => rl match {
+    case Tree(Red, Leaf(), ck, cv, Leaf()) => 
+      Tree(c, l, k, v, Tree(Black, Leaf(), ck, cv, Leaf()))
+  }
+  // case 1.4
+  case Tree(c, l, k, v, Tree(Black, Leaf(), rk, rv, rr))
+  if (implicitly[Ordering[B]].eq(rk, key)) => rr match {
+    case Tree(Red, Leaf(), ck, cv, Leaf()) => 
+      Tree(c, l, k, v, Tree(Black, Leaf(), ck, cv, Leaf()))
+  }
+  // case 1.5
+  case Tree(c, Tree(Red, ll, lk, lv, Leaf()), k, v, r)
+  if (implicitly[Ordering[B]].eq(lk, key)) => ll match {
+    case Tree(Black, Leaf(), ck, cv, Leaf()) =>
+      Tree(c, Tree(Black, Leaf(), ck, cv, Leaf()), k, v, r)
+  }
+  // case 1.6
+  case Tree(c, Tree(Red, Leaf(), lk, lv, lr), k, v, r)
+  if (implicitly[Ordering[B]].eq(lk, key)) => lr match {
+    case Tree(Black, Leaf(), ck, cv, Leaf()) =>
+      Tree(c, Tree(Black, Leaf(), ck, cv, Leaf()), k, v, r)
+  }
+  // case 1.7
+  case Tree(c, l, k, v, Tree(Red, Leaf(), rk, rv, rr))
+  if (implicitly[Ordering[B]].eq(rk, key)) => rr match {
+    case Tree(Black, Leaf(), ck, cv, Leaf()) =>
+      Tree(c, l, k, v, Tree(Black, Leaf(), ck, cv, Leaf()))
+  }
+  // case 1.8
+  case Tree(c, l, k, v, Tree(Red, rl, rk, rv, Leaf()))
+  if (implicitly[Ordering[B]].eq(rk, key)) => rl match {
+    case Tree(Black, Leaf(), ck, cv, Leaf()) =>
+      Tree(c, l, k, v, Tree(Black, Leaf(), ck, cv, Leaf()))
   }
 
-  render() {
-    const { currentView, currentViewData } = this.props;
-    return(
-      let dataList;
-      if (currentView === DATA_LIST_VOTATIONS_VIEW ||
-          currentView === DATA_LIST_USER_VOTATIONS_VIEW ||
-          currentView === DATA_LIST_VOTATIONS_SEARCH_VIEW) {
-      
-        datalist = currentViewData.map((item) => {
-          return <VotationCard />
-        });
-      } else if (currentView === DATA_LIST_USERS_SEARCH_VIEW) {
-        dataList = currentViewData.map((item) => {
-          return <UserCard />
-        });
-      }
-    );
-  }
-
-  _onScroll(event) {
-    const { currentPaginationOffset, currentView } = this.props;
-
-    let userId;
-    if (currentView === DATA_LIST_USER_VOTATIONS_VIEW) {
-      userId = Number.parseInt(location.split('/')[2])
-    }
-
-    this.props.dispatchLastRequest(currentView, {
-      offset: currentPaginationOffset, 
-      limit: DEFAULT_PAGINATION_LIMIT, 
-      userId
-    });
-  }
-}
-
-// возможно setCurView и setPaginationOffset выставлять внутри соотв. thunk`а?
-// each of 4 types requests should set lastRequest dataList state variable
-
-function mapStateToProps({ userReducer, votationsReducer, dataListReducer }) {
-  return {
-    currentView: dataListReducer.currentView,
-    currentPaginationOffset: dataListReducer.currentPaginationOffset
-  };
-}
-
-DataList.prototypes = {
-  getVotations: PropTypes.func.isRequired,
-  getUserVotations: PropTypes.func.isRequired,
-  searchVotations: PropTypes.func.isRequired,
-
-  searchUsers: PropTypes.func.isRequired,
-
-  currentlySendingPaginationReq: PropTypes.bool.isRequired,
-  currentView: PropTypes.bool.isRequired,
-  currentViewData: PropTypes.bool.isRequired,
-  currentPaginationOffset: PropTypes.number.isRequired,
-  sendingPaginationRequest: PropTypes.bool.isRequired,  
-}
-
-// ./actions/votationsActions.js
-import { 
-  setCurView, 
-  setCurViewData,
-  setPaginationOffset,
-  sendingPaginationRequest
-} from '../actions/dataListActions.js';
-import {
-  DATA_LIST_VOTATIONS_VIEW,
-  DATA_LIST_USER_VOTATIONS_VIEW,
-  DATA_LIST_VOTATIONS_SEARCH_VIEW,
-  DATA_LIST_USERS_SEARCH_VIEW
-} from '../constants/dataListConstants.js'
-import { API_BASE_PATH } from '../../config.js';
-
-export function getVotations(data) {
-  return (dispatch, getState) {    
-    if (getState().currentView !=== DATA_LIST_VOTATIONS_VIEW) {
-      dispatch(setCurViewData([]));
-      dispatch(setCurView(DATA_LIST_VOTATIONS_VIEW));
-    }
-
-    dispatch(sendingPaginationRequest(true));
-    
-    let _status;
-
-    fetch(API_BASE_PATH + '/votations' + constructQuery(data), {
-      headers: {
-        'Accept': 'application/json, text/plain, */*',
-        'Content-Type': 'application/json'
-      },
-      mode: 'cors',
-      method: 'GET',
-      credentials: 'include',
-    })
-    .then(res => {
-      _status = res.status;
-      return res.json();
-    })
-    .then((json) => { 
-      let data = JSON.parse(json);
-
-      if (_status === 200) {
-        dispatch(setCurViewData(data));
-
-      } else if (_status === 400) {
-        // TODO
-      } else {
-        // TODO
-      }
-      dispatch(sendingPaginationRequest(false));
-    })
-    .catch((err) => {
-        dispatch(setErrorMessage('login', commonErrors.ERROR));
-        dispatch(sendingLoginRequest(false));
-    });
-  };
-}
-
-// ./constants/dataListConstants.js
-'use strict';
-
-module.exports =  {
-  DATA_LIST_VOTATIONS_VIEW: 'DATA_LIST_VOTATIONS_VIEW',
-  DATA_LIST_USER_VOTATIONS_VIEW: 'DATA_LIST_USER_VOTATIONS_VIEW',
-  DATA_LIST_VOTATIONS_SEARCH_VIEW: 'DATA_LIST_VOTATIONS_SEARCH_VIEW',
-  DATA_LIST_USERS_SEARCH_VIEW: 'DATA_LIST_USERS_SEARCH_VIEW',
-
-  SET_CUR_VIEW: 'SET_CUR_VIEW',
-  SET_CUR_VIEW_DATA: 'SET_CUR_VIEW_DATA',
-  SENDING_PAGINATION_REQUEST: 'SENDING_PAGINATION_REQUEST',
-  SET_PAGINATION_OFFSET: 'SET_PAGINATION_OFFSET'
-}
-
-// .actions/dataListActions.js
-import { 
-  getVotations, 
-  getUserVotations, 
-  searchVotations 
-} from '../actions/votationsActions.js';
-import { searchUsers } from '../actions/userActions.js';
-
-export function dispatchLastRequest(view, paginationOffset, limit, data) {
-  return (dispatch, getState) => {
-    let request;
-    switch(view) {
-      case DATA_LIST_VOTATIONS_VIEW:
-        request = getVotations;
-      case DATA_LIST_USER_VOTATIONS_VIEW:
-        request = getUserVotations;
-      case DATA_LIST_VOTATIONS_SEARCH_VIEW:
-        request = searchVotations;
-      case DATA_LIST_USERS_SEARCH_VIEW:
-        request = searchUsers;
-      default:
-        request = getVotations;
-    }
-
-    dispatch(request(paginationOffset, limit, data));
-  };
-}
-
-export function setCurView(view) {
-  return { type: SET_CUR_VIEW, currentView: view }
-}
-
-export function setCurViewData(data) {
-  return { type: SET_CUR_VIEW_DATA, currentViewData: data }
-}
-
-export function setPaginationOffset(offset) {
-  return { type: SET_PAGINATION_OFFSET, currentPaginationOffset: offset }
-  }
-}
-
-export function sendingPaginationRequest(sending) {
-  return { type: SENDING_PAGINATION_REQUEST, sendingPaginationRequest: sending };
-}
-
-// ./reducers/datalistReducer.js
-import {
-  DATA_LIST_VOTATIONS_VIEW,
-
-  SET_CUR_VIEW,
-  SET_CUR_VIEW_DATA,
-  SENDING_PAGINATION_REQUEST,
-  SET_PAGINATION_OFFSET
-} from '../constants/dataListConstants.js'
-
-import { 
-  DEFAULT_PAGINATION_OFFSET, 
-} from '../constants/commonConstants.js';
-
-const initialState = {
-  currentView: DATA_LIST_VOTATIONS_VIEW,
-  currentViewData: [],
-  currentPaginationOffset: DEFAULT_PAGINATION_OFFSET,
-  sendingPaginationRequest: false
-}
-
-const assign = Object.assign;
-
-export default datalistReducer(state = initialState, action) {
-  switch(action.type) {
-    case SET_CUR_VIEW:
-      return assign({}, state, {
-        currentView: action.currentView
-      });
-    case SET_CUR_VIEW_DATA: {
-      let newData = state.currentViewData;
-      newData.push(action.currentViewData);
-
-      return assign({}, state, {
-        currentViewData: newData
-      });
-    }
-    case SENDING_PAGINATION_REQUEST: 
-      return assign({}, state, {
-        sendingPaginationRequest: action.sendingPaginationRequest
-      });
-    case SET_PAGINATION_OFFSET:
-      return assign({}, state, {
-        currentPaginationOffset: actions.offset
-      });
-    default:
-      return state;
-  }
+  // case 2.a.1.1
+  case Tree(_, Tree(Black, Leaf(), lk, lv, Leaf()), k, v, 
+               Tree(Black, slc @ Tree(Red, Leaf(), slck, slcv, Leaf()), rk, rv, 
+                           src @ Tree(Red, Leaf(), srck, srcv, Leaf())))
+  if (implicitly[Ordering[B]].eq(lk, key)) => 
+    Tree(Black, 
+      Tree(Black, 
+        Leaf(), 
+        k, v, 
+        Tree(Red, Leaf(), slck, slcv, Leaf())
+      ), 
+      rk, rv, 
+      Tree(Black, Leaf(), srck, srcv, Leaf())
+    )
+  // case 2.a.1.2  
+  case Tree(_, Tree(Black, Leaf(), lk, lv, Leaf()), k, v, 
+               Tree(Black, slc @ Leaf(), rk, rv, 
+                           src @ Tree(Red, Leaf(), srck, srcv, Leaf())))
+  if (implicitly[Ordering[B]].eq(lk, key)) => 
+    Tree(Black, Tree(Black, Leaf(), k, v, Leaf())), 
+      rk, rv, 
+      Tree(Black, Leaf(), srck, srcv, Leaf())
+    )
+  // case 2.a.2
+  case Tree(_, Tree(Black, Leaf(), lk, lv, Leaf()), k, v, 
+               Tree(Black, slc @ Tree(Red, Leaf(), slck, slcv, Leaf()), rk, rv, 
+                           src @ Leaf()))
+  if (implicitly[Ordering[B]].eq(lk, key)) =>
+    Tree(Black, Tree(Black, Leaf(), k, v, Leaf()), slck, slcv, Tree(Black, Leaf(), rk, rv, Leaf())) 
+  // case 2.a.3.1
+  case Tree(Black, 
+    Tree(Black, 
+      slc @ Tree(Red, Leaf(), slck, slcv, Leaf())
+      lk, lv,
+      src @ Tree(Red, Leaf(), srck, srcv, Leaf())), 
+    k, v, 
+    Tree(Black, Leaf(), rk, rv, Leaf()))
+  if (implicitly[Ordering[B]].eq(rk, key)) => 
+    Tree(Black, 
+      Tree(Black, Leaf(), slck, slcv, Leaf()), 
+      lk, lv, 
+      Tree(Black,
+        Tree(Red, Leaf(), srck, srcv, Leaf())
+        k, v,
+        Leaf()
+      )
+    )
+  // case 2.a.3.2
+  case Tree(Black, 
+    Tree(Black, 
+      slc @ Tree(Red, Leaf(), slck, slcv, Leaf())
+      lk, lv,
+      src @ Leaf()), 
+    k, v, 
+    Tree(Black, Leaf(), rk, rv, Leaf()))
+  if (implicitly[Ordering[B]].eq(rk, key)) => 
+    Tree(Black, 
+      Tree(Black, Leaf(), slck, slcv, Leaf()), 
+      lk, lv, 
+      Tree(Black,
+        Leaf()
+        k, v,
+        Leaf()
+      )
+    )
+  // case 2.a.4
+  case Tree(Black,
+    Tree(Black, slc @ Leaf(), lk, lv, src @ Tree(Red, Leaf(), srck, srcv, Leaf()))
+    k, v,
+    Tree(Black, Leaf(), rk, rv, Leaf()))
+  if (implicitly[Ordering[B]].eq(rk, key)) => 
+    Tree(Black, 
+      Tree(Black, Leaf(), lk, lv, Leaf())
+      srck, srcv,
+      Tree(Black, Leaf(), k, v, Leaf())
+    )
+  // case 2.b.1
+  case Tree(Black, Tree(Black, Leaf(), lk, lv, Leaf()), k, v, Tree(Black, Leaf(), rk, rv, Leaf()))
+  if (implicitly[Ordering[B]].eq(lk, key)) =>
+    Tree(Black, Leaf(), k, v, Tree(Red, Leaf(), rk, rv, Leaf()))
+  // case 2.b.2
+  case Tree(Black, Tree(Black, Leaf(), lk, lv, Leaf()), k, v, Tree(Black, Leaf(), rk, rv, Leaf()))
+  if (implicitly[Ordering[B]].eq(rk, key)) =>
+    Tree(Black, Tree(Red, Leaf(), lk, lv, Leaf()), k, v, Leaf())
+  // case 2.b.3
+  case Tree(Red, Tree(Black, Leaf(), lk, lv, Leaf()), k, v, Tree(Black, Leaf(), rk, rv, Leaf()))
+  if (implicitly[Ordering[B]].eq(lk, key)) =>
+    Tree(Black, Leaf(), k, v, Tree(Red, Leaf(), rk, rv, Leaf()))
+  // case 2.b.4
+  case Tree(Red, Tree(Black, Leaf(), lk, lv, Leaf()), k, v, Tree(Black, Leaf(), rk, rv, Leaf()))
+  if (implicitly[Ordering[B]].eq(rk, key)) =>
+    Tree(Black, Tree(Red, Leaf(), lk, lv, Leaf()), k, v, Leaf())
+  // case 2.c.1
+  case Tree(Black, 
+    Tree(Black, Leaf(), lk, lv, Leaf()), 
+    k, v, 
+    Tree(Red, 
+      slc @ Tree(Black, Leaf(), slck, slcv, Leaf())
+      rk, rv,
+      src @ Tree(Blacl, Leaf(), srck, srcv, Leaf())
+    )
+  )
+  if (implicitly[Ordering[B]].eq(lk, key)) =>
+    Tree(Black, 
+      Tree(Black, Leaf(), k, v, Tree(Red, Leaf(), slck, slcv, Leaf())), 
+      rk, rv, 
+      Tree(Black, Leaf(), srck, srcv, Leaf())
+    )
+  // case 2.c.2
+  case Tree(Black, 
+    Tree(Black, Leaf(), lk, lv, Leaf())
+    k, v,
+    Tree(Red, 
+      Tree(Black, Leaf(), slck, slcv, Leaf()),
+      rk, rv,
+      Leaf()
+    )
+  )
+  if (implicitly[Ordering[B]].eq(lk, key)) =>
+    Tree(Black, Tree(Black, Leaf(), k, v, Leaf()), slck, slcv, Tree(Black, Leaf(), rk, rv, Leaf()))
+  // case 2.c.3
+  case Tree(Black, 
+    Tree(Black, Leaf(), lk, lv, Leaf()),
+    k, v,
+    Tree(Red,
+      Tree(Black, Leaf(), srck, srcv, Leaf())
+    )
+  )
+  if (implicitly[Ordering[B]].eq(lk, key)) =>
+    Tree(Black, Tree(Black, Leaf(), k, v, Leaf()), rk, rv, Tree(Black, Leaf(), srck, srcv, Leaf()))
+  // case 2.c.4
+  case Tree(Black,
+    Tree(Red,
+      Tree(Black, Leaf(), slck, slcv, Leaf()),
+      lk, lv,
+      Tree(Black, Leaf(), srck, srcv, Leaf())
+    ),
+    k, v,
+    Tree(Black, Leaf(), rk, rv, Leaf())
+  )
+  if (implicitly[Ordering[B]].eq(rk, key)) =>
+    Tree(Black, 
+      Tree(Black, Leaf(), slck, slcv, Leaf()),
+      lk, lv,
+      Tree(Black,
+        Tree(Red, Leaf(), srck, srcv, Leaf()),
+        k, v,
+        Leaf()
+      )
+    )
+  // case 2.c.5
+  case Tree(Black,
+    Tree(Red, 
+      Tree(Black, Leaf(), slck, slcv, Leaf()),
+      lk, lv,
+      Leaf()
+    ),
+    k, v,
+    Tree(Black, Leaf(), rk, rv, Leaf())
+  )
+  if (implicitly[Ordering[B]].eq(rk, key)) =>
+    Tree(Black, Tree(Black, Leaf(), slck, slcv, Leaf()), lk, lv, Tree(Black, Leaf(), k, v, Leaf()))
+  // case 2.c.6
+  case Tree(Black,
+    Tree(Red, 
+      Leaf(),
+      lk, lv,
+      Tree(Black, Leaf(), srck, srcv, Leaf())
+    ),
+    k, v,
+    Tree(Black, Leaf(), rk, rv, Leaf())
+  )
+  if (implicitly[Ordering[B]].eq(rk, key)) =>
+    Tree(Black, Tree(Black, Leaf(), srck, srcv, Leaf()), lk, lv, Tree(Black, Leaf(), k, v, Leaf()))
 }
