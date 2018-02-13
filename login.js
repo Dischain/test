6e62eb43eca4569e8d8e1993db707419ae13b468
Aleksandr Shaytanov <rdischain@yandex.ru>
2018-02-13 14:32:06 +0000

    Update login.js

--- login.js
+++ login.js
@@ -1,39 +1,60 @@
-def insert(k: A, v: B): Option[B] = {
-  def insertR(node: SLNode[A, B], newNode: SLNode[A, B], level: Int): Option[B] = {
-    val key = newNode.key
-    val next = node.forward(level)
-
-    if (next.isEmpty || implicitly[Ordering[A]].lt(key, next.key)) {
-      if (level < newNode.level) { 
-        newNode.forward (level) = next 
-        node.forward (level) = newNode 
-      } 
-      if (level != 0) insertR(node, newNode, level - 1) 
-      else Some(v) 
-    }
-    else insertR(next, newNode, level)
-  }
-  
-  find(k) match {
-    case Some(item) => None
-    case _ => insertR(head, SLNode[A, B](k, v, randomLevel()), numLevels)
-  }  
+/** 
+ * Example:
+ * {{{
+ * implicit val personReadable: = new Readable[Person] {
+ *   def read(pVal: JsonVal): Option[Person] = {
+ *     for{
+ *       name <- pVal </> "name"
+ *       age  <- pVal </> "age"
+ *     } Option(Person(name, age))
+ *   }  
+ * }
+ *
+ * val personStr = """{"name": "John", "age": "22"}""" 
+ * val personJsVal: JsonVal = Json.parse(personStr)
+ * val person: Person = personJsVal.as[Person]
+ * }}}
+ 
+trait Readable[A] {
+  def read(jsonVal: JsonVal): Option[A]
+}
+trait JsonTransformable {
+  def as[T](implicit readable: Readable[T]): Option[T] = readable.read()
 }
 
-private var head: SLNode[A, B] = new SLNode[A, B](maxLevel)
+sealed trait JsonVal extends JsonTransformable {
 
-class SLNode[A: Ordering, B] (val key: A, val value: B, val level: Int, val isEmpty: Boolean = false) { 
-  def this(level: Int) { 
-    this(null.asInstanceOf[A], null.asInstanceOf[B], level, true) 
-  } 
-  
-  def this() { 
-    this(null.asInstanceOf[A], null.asInstanceOf[B], -1, true) 
-  } 
- 
-  val forward: Array[SLNode[A, B]] = 
-    Array.fill[SLNode[A, B]](level)(new SLNode[A, B]()) 
- 
-  def apply(key: A, value: B, level: Int): SLNode[A, B] = 
-    new SLNode[A, B](key, value, level) 
-} 
+}
+
+case object JsonNull extends JsonValue
+
+sealed trait JsonBoolean (val value: Boolean) extends JsonVal
+case object JsonTrue extends JsonBoolean(true)
+case object JsonFalse extends JsonBoolean(false)
+
+case class JsonNumber(value: Long) extends JsonVal
+
+case class JsonString(value: String) extends JsonVal
+
+case class JsonArray(value: IndexedSeq[JsonVal] = Array[JsonValue]()) extends JsonVal {
+  def ++(other: JsonArray): JsonArray = JsonArray(value ++ other.value)
+  def :+(el: JsonValue): JsonArray = JsonArray(value :+ el)
+  def +:(el: JsonValue): JsonArray = JsonArray(el +: value)
+}
+
+case class JsonObject(val fields: Map[String, JsonValue]) extends JsonValue {
+  lazy val fields: Seq[(String, JsonValue)] = underlying.toSeq
+  lazy val value: Map[String, JsonValue] = underlying match {
+    case m: immutable.Map[String, JsonValue] => m
+    case m => m.toMap
+  }
+
+  def fieldSet: Set[(String, JsonValue)] = fields.toSet
+  def keys: Set[String] = underlying.keySet
+  def values: Iterable[JsonValue] = underlying.values
+
+  def ++(other: JsonObject): JsonObject = JsonObject(underlying ++ other.underlying)
+  def -(otherField: String): JsonObject = JsonObject(underlying - otherField)
+  def +(otherField: (String, JsonValue)): JsonObject =
+    JsonObject(underlying + otherField)
+}
