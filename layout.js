194501f8ab727c1d4be006e0fe5da998e8a9391b
Aleksandr Shaytanov <rdischain@yandex.ru>
2018-02-14 14:30:43 +0000

    Update layout.js

--- layout.js
+++ layout.js
@@ -1,103 +1,132 @@
-    /*Убрать color: #0366d6; из <a>-тега и переместить в votation-data__name*/
-    /************************************************************************************************/
-    .votations-filter { /*./components/_votations-filter.css*/
-      padding-top: 16px;
-      padding-bottom: 16px;
-      border-bottom: 1px #e1e4e8 solid;
-    }
-    
-    .votations-filter__sb-wrapper {
-      padding-right: 24px;
-      width: 99%;
-      display: table-cell;
-      white-space: nowrap;
-    }
-    .votations-filter__controls-wrapper {
-      display: table-cell;
-      width: 1%;
-      position: relative;
-      white-space: nowrap;
-    }
-   
-    .search-box { /*./components/_search-box.css*/
-      width: 100%;
-      min-height: 34px;
-      box-sizing: border-box;
-      border-radius: 2px;
-      font-size: 16px;
-      padding-left: 25px;
-      border: 1px solid #6CC0E5;
-      background-color: #fff;
-      background-image: url('search-icon-sm.png');
-      background-repeat: no-repeat;
-      background-position: 5px 5px;
-    }
-    .select-list { /*./compoenents/_select-list.css*/
-      border: 1px solid #6CC0E5;
-      background: transparent;
-      border-radius: 3px;
-      width: auto;
-      padding: 5px 10px 5px 5px;
-      font-size: 16px;
-      height: 34px;
-    }
-    .votations-btn {
-      padding: 7px;
-      margin-left: 16px;
-      color: #6CC0E5;
-      border: 1px solid #6CC0E5;
-      border-radius: 3px;
-      text-decoration: none;
-      user-select: none;
-      display: inline-block;
-    }
-    .votations-btn:hover {
-      color: white;
-      background-color: #6CC0E5;
-    }
-    .btn {
-      padding: 7px;
-      margin-left: 16px;
-      color: #6CC0E5 !important;
-      border: 1px solid #6CC0E5;
-      border-radius: 3px;
-      text-decoration: none;
-      user-select: none;
-      display: inline-block;
+/** 
+ * case class Person(name: String, adress: Address)
+ * case class Address(street: String, city: String)
+ * 
+ * implicit val addressRead: = new Readable[Address]{
+ *   def read(addr: JsonVal): Option[Address] = {
+ *     for {
+ *       street <- (addr </> "street") as[String], 
+ *       city <- (addr </> "city") as[String]
+ *     } Option(Address(street, city))
+ *   }
+ * }
+ */
+trait Readable[A] {
+  def read(jsonVal: JsonVal): Option[A]
+}
+
+object ImplicitConversions {
+  implicit val intRead = new Readable[Int] {
+    def read(jn: JsonNumber): Option[Int] = Some(jn.value.toInt)
+  }
+
+  implicit val floatRead = new Readable[Float] {
+    def read(jn: JsonNumber): Option[Float] = Some(jn.value.toFloat)
+  }
+
+  // ...
+}
+
+trait Writable[A] {
+  def write(object: A): JsonVal
+}
+
+trait JsonTransformable {
+  def as[T](implicit readable: Readable[T]): T = readable.read() match {
+    case Some(result) => result
+    case None => throw new Exception ...
+  }
+}
+
+final class Parser extends JavaTokenParsers {
+  def object: Parser[JsonObject] = 
+    "{"~> repsep(member, ",") <~"}" ^^ 
+      (JsonObject(Map[String, JsonVal]()) + _)
+
+  def arr: Parser[JsonArray] = 
+    "["~> repsep(value, ",") <~"]" 
+
+  def member: Parser[(Sting, JsonVal)] = 
+    stringLiteral~":"~value ^^ { 
+      case name~":"~value => (name, value) 
     }
 
-    .btn:hover {
-      color: white;
-      background-color: #6CC0E5;
-      cursor: pointer;
-    }
-    /************************************************************************************************/
-  </style>
-</head>
-<body>
-  <div class="container clearfix">   
-    <div class="content float-left">
-      <div class="position-relative">
-        <!--Votations Filter Start-->
-        <div class="votations-filter">
-          <form class="display-table">
-            <div class="votations-filter__sb-wrapper">
-              <input class="search-box" type="search" placeholder="Search votations..." autocomplete="off"></input>
-            </div>
-            <div class="votations-filter__controls-wrapper text-right">
-              <select class="select-list">
-                <option value="All">All</option>
-                <option value="Owner">Owner</option>
-                <option value="Participated">Participated</option>
-              </select>
-              <div class="btn">
-                <a >New</a>
-              </div>
-            </div>            
-          </form>
-        </div>
-        <!--Votations Filter End-->        
-      </div>
-    </div>
-  </div>
-</body>
+  def value: Parser[JsonVal] = ( 
+    obj 
+      | arr
+      | stringLiteral ^^ (JsonString(_))
+      | longNumber ^^ (JsonNumber(_.toLong))
+      | "null"  ^^ (JsonNull) 
+      | "true"  ^^ (JsonTrue) 
+      | "false" ^^ (JsonFalse) 
+    )
+}
+
+object Parser extends Parser {
+  def parse(string: String): JsonVal = parseAll(string) match // ...
+}
+
+implicit class Path(jo: JsonObject) {
+  def </> [T](field: String): Option[JsonVal] = jo.value(field)
+}
+
+object Json {
+  def stringify(value: JsonVal): String = value.toString()
+
+  def parse(string: String): JsonVal = Parser.parse(string)
+
+  def toJson[T](obj: T)(implicit jw: Writable[T]): JsonVal = 
+    jw.write(obj)
+
+  def fromJson[T](value: JsonVal)(implicit jr: Readable[T]): Option[T] = 
+    jr.read(value)
+
+  def obj()
+}
+
+sealed trait JsonVal extends JsonTransformable
+
+case object JsonNull extends JsonVal {
+  override def toString: String = "\"null\""
+}
+
+sealed trait class JsonBoolean extends JsonVal
+
+case object JsonTrue extends JsonBoolean {
+  override def toString: String = "true"
+}
+case object JsonFalse extends JsonBoolean {
+  override def toString: String = "false"
+}
+
+case class JsonNumber(private[ds] value: Long) extends JsonVal {
+  override def toString: String = value.toString
+}
+
+case class JsonString(private[ds] value: String) extends JsonVal {
+  override def toString: String = "\"" + value + "\""
+}
+
+case class JsonArray(private[ds] value: IndexedSeq[JsonVal] = Array[JsonVal]()) 
+extends JsonVal 
+{
+  // ...
+
+  override def toString: String = value.mkString(",")
+}
+
+case class JsonObject(private[ds] val fields: Map[String, JsonVal]) extends JsonVal {
+  // ...  
+  override def toString: String = fields.toSeq.mkString(", ")  
+}
+
+object JsonObject {
+  def apply(members: (String, JsonVal)*): JsonObject = 
+    member foldLeft
+      (JsonObject(Map[JsonString, JsonObject]()))
+      ((init: JsonObject, current: [JsonString -> JsonObject]) =>
+      {
+        init + current
+      }
+    )
+}
