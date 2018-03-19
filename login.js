87029031a505393924bef031d25ccf248825e234
Aleksandr Shaytanov <rdischain@yandex.ru>
2018-03-19 14:35:43 +0000

    Update login.js

--- login.js
+++ login.js
@@ -1,60 +1,54 @@
-/** 
- * Example:
- * {{{
- * implicit val personReadable: = new Readable[Person] {
- *   def read(pVal: JsonVal): Option[Person] = {
- *     for{
- *       name <- pVal </> "name"
- *       age  <- pVal </> "age"
- *     } Option(Person(name, age))
- *   }  
- * }
- *
- * val personStr = """{"name": "John", "age": "22"}""" 
- * val personJsVal: JsonVal = Json.parse(personStr)
- * val person: Person = personJsVal.as[Person]
- * }}}
- 
-trait Readable[A] {
-  def read(jsonVal: JsonVal): Option[A]
-}
-trait JsonTransformable {
-  def as[T](implicit readable: Readable[T]): Option[T] = readable.read()
-}
-
-sealed trait JsonVal extends JsonTransformable {
-
-}
-
-case object JsonNull extends JsonValue
-
-sealed trait JsonBoolean (val value: Boolean) extends JsonVal
-case object JsonTrue extends JsonBoolean(true)
-case object JsonFalse extends JsonBoolean(false)
-
-case class JsonNumber(value: Long) extends JsonVal
-
-case class JsonString(value: String) extends JsonVal
-
-case class JsonArray(value: IndexedSeq[JsonVal] = Array[JsonValue]()) extends JsonVal {
-  def ++(other: JsonArray): JsonArray = JsonArray(value ++ other.value)
-  def :+(el: JsonValue): JsonArray = JsonArray(value :+ el)
-  def +:(el: JsonValue): JsonArray = JsonArray(el +: value)
-}
-
-case class JsonObject(val fields: Map[String, JsonValue]) extends JsonValue {
-  lazy val fields: Seq[(String, JsonValue)] = underlying.toSeq
-  lazy val value: Map[String, JsonValue] = underlying match {
-    case m: immutable.Map[String, JsonValue] => m
-    case m => m.toMap
+// закинуть переменные доменов в env
+// domain - "kometa_tuning_simferopol"
+class VkActor (token: String, domain: String) extends Actor {
+  def recieve = {
+    case LookupNewPost =>
+      vk.wall
+        .get(domain, Some(2), Some(1))
+        .map {
+          case r: WallGetResponse => r.items map { 
+            item => sender ! VkPostRecieved(item) 
+          }
+          
+          case VkApiFailure => sender ! VkFailure
+        }
   }
+}
 
-  def fieldSet: Set[(String, JsonValue)] = fields.toSet
-  def keys: Set[String] = underlying.keySet
-  def values: Iterable[JsonValue] = underlying.values
+// domain - @vttbottest
+class TelegramActor (token: String, domain: String) extends Actor {
+  def recieve = {
+    case TelegramSendMessage(post: VkWallPost) =>
+      tb.sendMessage(domain, formPost(post, domain), Some(ParseMode.HTML))
+        .map {
+          case SuccessfullyDelivered => sender ! UpdateLastPostId(post.id)
+          case Failed => sender ! Tick
+        }
+  }
+}
 
-  def ++(other: JsonObject): JsonObject = JsonObject(underlying ++ other.underlying)
-  def -(otherField: String): JsonObject = JsonObject(underlying - otherField)
-  def +(otherField: (String, JsonValue)): JsonObject =
-    JsonObject(underlying + otherField)
+class VtTScheduler (
+                    vkActor: ActorRef, 
+                    telegramActor: ActorRef
+                   ) extends Actor {
+  val delay = 5
+  var lastVkPostId: Int = _
+  
+  context.system.scheduler.scheduleOnce(delay minutes, self, Tick)
+
+  def recieve = {
+    case Tick =>
+      vkActor ! LookupNewPost
+    case VkPostRecieved(post: VkWallPost) =>
+      if (lastVkPostId != post.id) {
+        telegramActor ! TelegramSendMessage(post)
+      } else {
+        context.system.scheduler.scheduleOnce(delay minutes, self, Tick)
+      }
+    case VkFailure => context.system.scheduler.scheduleOnce(delay minutes, self, Tick)
+    case UpdateLastPostId(id: Int) => {
+      lastVkPostId = id
+      context.system.scheduler.scheduleOnce(delay minutes, self, Tick)
+    }
+  }
 }
