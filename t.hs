a0dbc0e4234067cc3249eb7b7f543303a4e7d3f8
Aleksandr Shaytanov <rdischain@yandex.ru>
2019-01-13 00:41:29 +0300

    Update t.hs

--- t.hs
+++ t.hs
@@ -1,10 +1,15 @@
 module TS where
 
+module T2 where
+
 data Column a = Column [a] deriving Show
 
-joinC:: [(Column a)] -> Column a
+joinC :: [(Column a)] -> Column a
 joinC cols@((Column _):cs) = 
   foldl (\(Column acc) (Column [a]) -> Column (a:acc)) (Column []) (reverse cols)
+
+concatC :: Column a -> Column a -> Column a
+concatC (Column a) (Column b) = Column (a ++ b)
 
 instance Functor Column where
   fmap f (Column []) = Column []
@@ -14,14 +19,15 @@ instance Applicative Column where
   pure a = Column [a]
 
   (Column []) <*> _ = Column []
-  (Column [f]) <*> col = fmap f col
-  
+  (Column (f : fs)) <*> (Column [c]) = Column [f c]
+  (Column (f : fs)) <*> (Column (c : cs)) = 
+    Column [f c] `concatC` (Column fs <*> Column cs)
+
 instance Monad Column where
   (>>=) (Column []) f = Column []
   (>>=) (Column c) f = joinC $ fmap f c
 
   return a = Column [a]
-
 
 ---------------
 data Table a = Empty | ConsT (Column a) (Table a) deriving Show
